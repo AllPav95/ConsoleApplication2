b8e0806dd3769cb065ebbdfea847080f746e0298
AllPav95 <136779078+AllPav95@users.noreply.github.com>
2023-09-03 20:19:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm> 
+
+int main() {
+const int arraySize = 26;
+char alphabetArray[arraySize];
+
+for (int i = 0; i < arraySize; i++) {
+    alphabetArray[i] = 'A' + i;
+}
+//for (int i = 0; i < arraySize; i++) {
+//    std::cout << 'A' << i + 1 << ": " << alphabetArray[i] << std::endl;
+//}
+std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+std::random_shuffle(std::begin(alphabetArray), std::end(alphabetArray));
+
+int i = 0;
+do {
+    char currentChar = alphabetArray[i];
+    if (currentChar == '_') {
+        std::cout << "  A" << i + 1 << ": " << "_" << std::endl;
+    }
+    else {
+        std::cout << "A" << i + 1 << ": " << currentChar << std::endl;
+    }
+    ++i;
+} while (i < arraySize);
+
+return 0;
+}
